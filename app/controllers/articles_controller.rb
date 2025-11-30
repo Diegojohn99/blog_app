fec21dc1d6b422374a7efa0938163fbf05d1808b
Diegojohn99 <diegodemon2019@gmail.com>
2025-11-29 20:43:00 -0400

    mejorando el responsive de la aplicacion y la carga de imagenes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,14 @@ class ArticlesController < ApplicationController
   def create
     @article = current_user.articles.new(article_params)
     
+    # Handle cropped image (base64 data)
+    if params[:article][:cropped_image].present?
+      process_cropped_image(@article, params[:article][:cropped_image])
+    # Handle image URL
+    elsif params[:article][:image_url].present? && !params[:article][:image].present?
+      process_image_url(@article, params[:article][:image_url])
+    end
+    
     if @article.save
       redirect_to @article, notice: 'Artículo creado exitosamente.'
     else
@@ -26,6 +34,14 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    # Handle cropped image (base64 data)
+    if params[:article][:cropped_image].present?
+      process_cropped_image(@article, params[:article][:cropped_image])
+    # Handle image URL
+    elsif params[:article][:image_url].present? && !params[:article][:image].present?
+      process_image_url(@article, params[:article][:image_url])
+    end
+    
     if @article.update(article_params)
       redirect_to @article, notice: 'Artículo actualizado exitosamente.'
     else
@@ -42,5 +58,51 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :content, :image, :image_style, :image_url)
+  end
+
+  def process_cropped_image(article, base64_data)
+    return unless base64_data.present?
+    
+    # Extract the base64 data and content type
+    if base64_data.match(/^data:image\/(\w+);base64,(.*)/)
+      content_type = "image/#{$1}"
+      encoded_data = $2
+      
+      # Decode the base64 data
+      decoded_data = Base64.decode64(encoded_data)
+      
+      # Create a temporary file
+      filename = "cropped_image_#{Time.now.to_i}.#{$1}"
+      
+      # Attach the image
+      article.image.attach(
+        io: StringIO.new(decoded_data),
+        filename: filename,
+        content_type: content_type
+      )
+    end
+  rescue => e
+    Rails.logger.error "Error processing cropped image: #{e.message}"
+  end
+
+  def process_image_url(article, url)
+    return unless url.present?
+    
+    require 'open-uri'
+    
+    # Download the image from URL
+    downloaded_image = URI.open(url)
+    
+    # Extract filename from URL or use a default
+    filename = File.basename(URI.parse(url).path)
+    filename = "image_#{Time.now.to_i}.jpg" if filename.blank?
+    
+    # Attach the image
+    article.image.attach(
+      io: downloaded_image,
+      filename: filename
+    )
+  rescue => e
+    Rails.logger.error "Error processing image URL: #{e.message}"
   end
 end
